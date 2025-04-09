33b34782283b6d67a8f0e7112b68480b25863d34
Naryshkin, Ilia <ilia_naryshkin@epam.com>
2025-04-09 14:38:22 +0300

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+# terraform apply -var "instance_name=YetAnotherName"
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
